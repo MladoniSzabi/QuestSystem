c97d790571e03e364b59d1a9eedac3ab9f359b69
MladoniSzabi <szabimlado@gmail.com>
2023-06-10 13:03:19 +0100

    added code to connect to the databases.

--- src/include/QuestSystem.hpp
+++ src/include/QuestSystem.hpp
@@ -1,12 +1,23 @@
 #ifndef QUESTSYSTEM_HPP_
 #define QUESTSYSTEM_HPP_
 
+#include <string>
+#include <vector>
+
+#include <sqlite/sqlite3.h>
+
+#include "Quest.hpp"
+
 class QuestSystem
 {
 private:
+    sqlite3 *_questDatabaseConn;
+
 public:
     QuestSystem();
     ~QuestSystem();
+
+    bool open(std::string questDatabaseFile);
 };
 
 #endif // QUESTSYSTEM_HPP_
--- src/src/QuestSystem.cpp
+++ src/src/QuestSystem.cpp
@@ -7,3 +7,13 @@ QuestSystem::QuestSystem()
 QuestSystem::~QuestSystem()
 {
 }
+
+bool QuestSystem::open(std::string questDatabaseFile)
+{
+    int error = sqlite3_open(questDatabaseFile.c_str(), &_questDatabaseConn);
+    if (error)
+    {
+        return false;
+    }
+    return true;
+}
